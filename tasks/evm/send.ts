feeed366b1f0b8f1f9ddd02ca8f165f611a656c8
mudassir <mudassirali36@gmail.com>
2025-06-03 17:15:56 +0500

    fix: add contractAddress in params

--- tasks/evm/send.ts
+++ tasks/evm/send.ts
@@ -12,45 +12,85 @@ interface TaskArguments {
     dstEid: number
     amount: string
     to: string
-    contractName: string
+    contractName?: string
+    contractAddress?: string
 }
 
 const action: ActionType<TaskArguments> = async (
-    { dstEid, amount, to, contractName },
+    { dstEid, amount, to, contractName, contractAddress },
     hre: HardhatRuntimeEnvironment
 ) => {
-    const signer = await hre.ethers.getNamedSigner('deployer')
-    // @ts-ignore
-    const token = (await hre.ethers.getContract(contractName)).connect(signer)
+    try {
+        const signer = await hre.ethers.getNamedSigner('deployer')
+        console.log('Signer address:', signer.address)
+        
+        // Initialize token contract either from address or name
+        let token
+        if (contractAddress) {
+            // @ts-ignore
+            token = (await hre.ethers.getContractAt(contractName || 'MyOFT', contractAddress)).connect(signer)
+        } else {
+            // @ts-ignore
+            token = (await hre.ethers.getContract(contractName || 'MyOFT')).connect(signer)
+        }
+        console.log('Token contract address:', token.address)
 
-    // if (isSepolia(hre.network.name)) {
-    //     // @ts-ignore
-    //     const erc20Token = (await hre.ethers.getContractAt(IERC20, address)).connect(signer)
-    //     const approvalTxResponse = await erc20Token.approve(token.address, amount)
-    //     const approvalTxReceipt = await approvalTxResponse.wait()
-    //     console.log(`approve: ${amount}: ${approvalTxReceipt.transactionHash}`)
-    // }
+        // Check token balance
+        const balance = await token.balanceOf(signer.address)
+        console.log('Token balance:', balance.toString())
+        console.log('Amount to send:', amount)
 
-    const amountLD = BigNumber.from(amount)
-    const sendParam = {
-        dstEid,
-        to: makeBytes32(bs58.decode(to)),
-        amountLD: amountLD.toString(),
-        minAmountLD: amountLD.mul(9_000).div(10_000).toString(),
-        extraOptions: '0x',
-        composeMsg: '0x',
-        oftCmd: '0x',
+        const amountLD = BigNumber.from(amount)
+        const sendParam = {
+            dstEid,
+            to: makeBytes32(bs58.decode(to)),
+            amountLD: amountLD.toString(),
+            minAmountLD: amountLD.mul(9_000).div(10_000).toString(),
+            extraOptions: '0x',
+            composeMsg: '0x',
+            oftCmd: '0x',
+        }
+        console.log('Send parameters:', sendParam)
+
+        // Get quote
+        const [msgFee] = await token.functions.quoteSend(sendParam, false)
+        console.log('Message fee:', {
+            nativeFee: msgFee.nativeFee.toString(),
+            lzTokenFee: msgFee.lzTokenFee.toString()
+        })
+
+        // Check native token balance
+        const nativeBalance = await signer.getBalance()
+        console.log('Native token balance:', nativeBalance.toString())
+        console.log('Required native fee:', msgFee.nativeFee.toString())
+
+        const txResponse = await token.functions.send(sendParam, msgFee, signer.address, {
+            value: msgFee.nativeFee,
+            gasLimit: 500_000,
+        })
+        console.log('Transaction sent:', txResponse.hash)
+        
+        const txReceipt = await txResponse.wait()
+        console.log(`send: ${amount} to ${to}: ${txReceipt.transactionHash}`)
+        console.log(
+            `Track cross-chain transfer here: ${getLayerZeroScanLink(txReceipt.transactionHash, dstEid == EndpointId.SOLANA_V2_TESTNET)}`
+        )
+    } catch (error: any) {
+        console.error('Error details:', {
+            message: error.message,
+            reason: error.reason,
+            code: error.code,
+            transactionHash: error.transactionHash,
+            transaction: error.transaction ? {
+                from: error.transaction.from,
+                to: error.transaction.to,
+                value: error.transaction.value?.toString(),
+                gasLimit: error.transaction.gasLimit?.toString(),
+                data: error.transaction.data?.slice(0, 66) + '...' // Show just the function selector
+            } : undefined
+        })
+        throw error
     }
-    const [msgFee] = await token.functions.quoteSend(sendParam, false)
-    const txResponse = await token.functions.send(sendParam, msgFee, signer.address, {
-        value: msgFee.nativeFee,
-        gasLimit: 500_000,
-    })
-    const txReceipt = await txResponse.wait()
-    console.log(`send: ${amount} to ${to}: ${txReceipt.transactionHash}`)
-    console.log(
-        `Track cross-chain transfer here: ${getLayerZeroScanLink(txReceipt.transactionHash, dstEid == EndpointId.SOLANA_V2_TESTNET)}`
-    )
 }
 
 task('send', 'Sends a transaction', action)
@@ -58,3 +98,4 @@ task('send', 'Sends a transaction', action)
     .addParam('amount', 'Amount to send in wei', undefined, types.string, false)
     .addParam('to', 'Recipient address', undefined, types.string, false)
     .addOptionalParam('contractName', 'Name of the contract in deployments folder', 'MyOFT', types.string)
+    .addOptionalParam('contractAddress', 'Address of the deployed contract', undefined, types.string)
